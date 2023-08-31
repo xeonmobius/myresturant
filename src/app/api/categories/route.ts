c78c46e4dd3f2781b2dfc07e34fe12a3067ede5e
xeonmobius <refinedzeta@gmail.com>
2023-08-31 08:32:33 -0400

    feat: created routes api and prisma migration

---
+++ src/app/api/categories/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/utils/connect";
+
+
+// Fetch all Categories
+export const GET = async () => {
+  try {
+    const categories = await prisma.category.findMany();
+    return new NextResponse(
+        JSON.stringify(categories),
+        {
+          status: 200,
+        }
+      );
+  } catch (error) {
+    console.log(error);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong" }),
+      {
+        status: 500,
+      }
+    );
+  }
+};
+
+export const POST = () => {
+  return new NextResponse("Hello", {
+    status: 200,
+  });
+};
